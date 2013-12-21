23f9eb6ab2cc836304e8ec8c52d835d77ca16a61
Kian Moretz <meecoder@users.noreply.github.com>
2013-12-21 07:17:56 -0500

    add random letter dictionary to encoder

--- python/encode.py
+++ python/encode.py
@@ -1,6 +1,11 @@
 from collections import defaultdict
+from random import randint
 
 def encode():
+	number2letter = {1 : 'a', 2 : 'b', 3 : 'c', 4 : 'd', 5 : 'e', 6 : 'f', 7 : 'g',
+	8 : 'h', 9 : 'i', 10 : 'j', 11 : 'k', 12 : 'l', 13 : 'm', 14 : 'n', 15 : 'o', 16 : 'p', 17 : 'q',
+	18 : 'r', 19 : 's', 20 : 't', 21 : 'u', 22 : 'v', 23 : 'w', 24 : 'x', 25 : 'y', 26 : 'z'}
+	
 	encode = {"a": "y", "b": "z", "c": "a", "d": "b", "e": "c", "f": "d", "g": "e",
 	"h": "f", "i": "g", "j": "h", "k": "i", "l": "j", "m": "k", "n": "l", "o": "m", 
 	"p": "n", "q": "o", "r": "p", "s": "q", "t": "r", "u": "s", "v": "t", "w": "u", 
